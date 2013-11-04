ccea1c637dd10b58300a8d5bd46affc386b593ba
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2013-11-04 19:12:55 +0000

    folds now test the final four numbers

--- fold.m
+++ fold.m
@@ -6,8 +6,10 @@ function [ data ] = fold( examples , labels )
     data.test = struct([]);
     data.train =struct([]);
     for i = 0:9
-       start = jump*i + 1; %1,101,201,
-       finish   = jump*(i+1);%100,200,300
+       start  = jump*i + 1; %1,101,201,
+       finish = jump*(i+1);%100,200,300
+       if (i==9) finish = num_exs;
+        end
        data.test(i+1).labels = labels(start:finish);
        data.test(i+1).examples = examples(start:finish,:);
        data.train(i+1).labels = cat(1,labels(1:start-1),labels(finish+1:end));
