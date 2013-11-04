bb49c94ae43164704789240b0971e6ce7a21c498
wrd11 <wrd11@doc.ic.ac.uk>
2013-11-04 18:43:04 +0000

    prediction works

--- predictions.m
+++ predictions.m
@@ -2,6 +2,6 @@ function [predictions] = classRatesTestTrees(dataset)
    folds = createFoldDecisionTrees(dataset.x,dataset.y);
    for i = 1:length(folds)
         test = testTrees(folds(i).trees,folds(i).test.examples);
-        predictions(i) = test; 
+        predictions{i} = test; 
    end
 end
