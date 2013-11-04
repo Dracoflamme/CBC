30fbd86c144455c5d7f22de1d8275788105d53a5
wrd11 <wrd11@doc.ic.ac.uk>
2013-11-04 20:21:11 +0000

    for realsies for realsies

--- fold.m
+++ fold.m
@@ -9,12 +9,12 @@ function [ data ] = fold( examples , labels )
     for i = 1:10
        start  = jump*(i-1) + 1; %1,101,201,
        finish = jump*(i); %100,200,300
-       if (i==10) finish = num_exs; %ensure we train on all data
+       if (i==10) finish = num_exs; %ensure we test on all data
         end
        data.test(i).labels = labels(start:finish-validation_size);
        data.test(i).examples = examples(start:finish-validation_size,:);
-       data.train(i).labels = cat(1,labels(1:start),labels(finish+1:end));
-       data.train(i).examples = cat(1,examples(1:start,:),examples(finish+1:end,:));
+       data.train(i).labels = cat(1,labels(1:start - 1),labels(finish+1:end));
+       data.train(i).examples = cat(1,examples(1:start- 1,:),examples(finish+1:end,:));
        data.validate(i).labels = labels(finish-validation_size+1:finish);
        data.validate(i).examples = examples(finish-validation_size+1:finish,:);
     end
