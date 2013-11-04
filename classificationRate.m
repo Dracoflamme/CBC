48bb1c79dced9a415c636b545b18471a76f13fea
Conrad Godfrey <conradjgodfrey@tiscali.co.uk>
2013-11-04 16:16:24 +0000

    Made classification rate actually correct

--- classificationRate.m
+++ classificationRate.m
@@ -1,8 +1,12 @@
-function [ rate ] = classificationRate( predictions, y )
+function [ rate ] = classificationRate(predictions, y )
 %Classification rate for a given classifier
 %   generates a confusion matrix and takes the number of true positives and
 %   true negatives as a proportion of the total number of examples
-    m = createConfusionMatrix(predictions, y);
-    rate = (m(1,1) + m(2,2))/length(predictions);
+    rate = 0;
+    for i=1:length(predictions)
+        if predictions(i) == y(i)
+            rate = rate + 1;
+        end
+    end
 end
 
