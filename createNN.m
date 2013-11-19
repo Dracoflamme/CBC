c490efd40b650ef61cf8e945c3c7df69c8fb8f04
Will de Renzy-Martin <wrd11@.ic.ac.uk>
2013-11-19 11:30:30 +0000

    made createNN a little clearer

--- createNN.m
+++ createNN.m
@@ -1,10 +1,21 @@
-function [ network ] = createNN( hidden_layers,training_fn, epochs, x,y,one_or_six)
-    [x2,y2] = ANNdata(x,y);
+function [ network ] = createNN( hidden_layers,training_fn, epochs, examples,labels,one_or_six)
+% inputs:
+% hidden_layers - either an array of required hidden layer nodes, or just a
+%                 number of nodes
+% training_fn   - e.g. 'trainlm'
+% epochs        - number of epochs in training
+% examples      - examples to train on
+% labels        - labels to train on
+% one_or_six    - 'one' - output a single NN
+%                 'six' - output six NNs, one for each label
+
+    [x2,y2] = ANNdata(examples,labels);
     if strcmp(one_or_six, 'one')
         network = makeNN(hidden_layers,training_fn,epochs,x2,y2);
     else
+        network = cell(6);
         for i = 1:6
-            ts = getBinaryTargets(y,i);
+            ts = getBinaryTargets(labels,i);
             network{i} = makeNN(hidden_layers,training_fn,epochs,x2,ts');
         end
     end
