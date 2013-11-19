56e581cb9f94dc665646886d410c751460cd2d14
Will de Renzy-Martin <wrd11@.ic.ac.uk>
2013-11-19 15:31:02 +0000

    paramOpts only takes 4 hours to run now.....

--- paramOpt.m
+++ paramOpt.m
@@ -1,9 +1,9 @@
 function [ topology, train_fn,learn_rate,momentum,activation_fn, epoch] = paramOpt( training_data,validation_data,one_or_six)
-    topologies = {40,30,20,10,[40,30],[40,20],[40,10],[30,20],[30,10],[20,10]};
-    training_fns = {'trainlm','trainscg','trainrp','traingdm'};
-    learn_rates = 0.1:0.1:1;
-    momentums = 0.1:0.1:0.9;
-    activation_fns = {'tansig','logsig','purelin'}; 
+    topologies = {40,20,10,[40,20],[30,10]};
+    training_fns = {'trainlm','trainrp','traingdm'};
+    learn_rates = 0.5:0.1:1;
+    momentums = 0.5:0.1:0.9;
+    activation_fns = {'tansig','logsig'}; 
     epochs = [10,100,1000];
     max_acc = 0;
     x = training_data.examples;
