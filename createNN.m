13bcbbdb243243260a56d6e36b0d01cce0649a3b
Will de Renzy-Martin <wrd11@.ic.ac.uk>
2013-11-19 15:12:32 +0000

    parameter optimisation works
    it's slow, but it does check 10*4*10*9*3*3 (32400) parameter
    configurations at the moment, obviously we can make that smaller...

--- createNN.m
+++ createNN.m
@@ -1,9 +1,12 @@
-function [ network ] = createNN( hidden_layers,training_fn, epochs, examples,labels,one_or_six)
+function [ network ] = createNN( hidden_layers,training_fn,learning_rate,momentum,activation_fn, epochs, examples,labels,one_or_six)
 % inputs:
 % hidden_layers - either an array of required hidden layer nodes, or just a
 %                 number of nodes
 % training_fn   - e.g. 'trainlm'
 % epochs        - number of epochs in training
+% learning_rate - learning rate
+% momentum      - momentum for gdm
+% activation_fn - activation function
 % examples      - examples to train on
 % labels        - labels to train on
 % one_or_six    - 'one' - output a single NN
@@ -11,20 +14,25 @@ function [ network ] = createNN( hidden_layers,training_fn, epochs, examples,lab
 
     [x2,y2] = ANNdata(examples,labels);
     if strcmp(one_or_six, 'one')
-        network = makeNN(hidden_layers,training_fn,epochs,x2,y2);
+        network = makeNN(hidden_layers,training_fn,learning_rate,momentum,activation_fn,epochs,x2,y2);
     else
         network = cell(1,6);
         for i = 1:6
             ts = getBinaryTargets(labels,i);
-            network{i} = makeNN(hidden_layers,training_fn,epochs,x2,ts');
+            network{i} = makeNN(hidden_layers,training_fn,learning_rate,momentum,activation_fn,epochs,x2,ts');
         end
     end
 end
 
-function [net] = makeNN(hidden_layers,train_fn,epochs,x,y)
+function [net] = makeNN(hidden_layers,train_fn,learn,mom, activation,epochs,x,y)
     [net] = feedforwardnet(hidden_layers,train_fn);
     [net] = configure(net,x,y);
     net.trainParam.epochs = epochs;
+    net.trainParam.lr = learn;
+    net.trainParam.mc = mom;
+    for i = 1: size(hidden_layers)
+        net.layers{i}.transferFcn = activation;
+    end
     [net] = train(net,x,y,'useParallel','yes','useGPU','only');
 end
 
--- paramOpt.m
+++ paramOpt.m
@@ -1,11 +1,39 @@
-function [ topology, train_fn, activation_fn, epoch,learn_rate,momentum] = paramOpt( training_data,validation_data,one_or_six)
-    training_fns = ['trainlm','trainscg','trainbr','trainrp','traingdm'];
-    activation_fns = ['tansig','logsig','purelin'];
-    topologies = [40,30,20,10,[40,30],[40,20],[40,10],[30,20],[30,10],[20,10]];
-    epochs = [10,100,1000];
+function [ topology, train_fn,learn_rate,momentum,activation_fn, epoch] = paramOpt( training_data,validation_data,one_or_six)
+    topologies = {40,30,20,10,[40,30],[40,20],[40,10],[30,20],[30,10],[20,10]};
+    training_fns = {'trainlm','trainscg','trainrp','traingdm'};
     learn_rates = 0.1:0.1:1;
     momentums = 0.1:0.1:0.9;
-    
-    
+    activation_fns = {'tansig','logsig','purelin'}; 
+    epochs = [10,100,1000];
+    max_acc = 0;
+    x = training_data.examples;
+    y = training_data.labels;
+    for top = 1:length(topologies)
+        for tra = 1:length(training_fns)
+            for le = 1:length(learn_rates)
+                for  mom = 1:length(momentums)
+                    for act = 1:length(activation_fns)
+                        for ep = 1:length(epochs)
+                            network = createNN(topologies{top},training_fns{tra},learn_rates(le),momentums(mom),activation_fns{act},epochs(ep),x,y,one_or_six);                   
+                            examples = validation_data.examples;
+                            out = sim(network,examples');
+                            test = NNout2labels(out);
+                            labels = validation_data.labels;
+                            accuracy = classificationRate(test,labels);
+                            if accuracy > max_acc
+                                max_acc = accuracy;
+                                topology = topologies{top};
+                                train_fn = training_fns{tra};
+                                learn_rate = learn_rates(le);
+                                momentum = momentums(mom);
+                                activation_fn = activation_fns{act};
+                                epoch = epochs(ep);
+                            end
+                        end
+                    end
+                end
+           end
+        end  
+    end
 end
 
