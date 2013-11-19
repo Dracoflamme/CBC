f0790095d6a8d2ac66f26367783abe9eca6771af
Guillaume Lacroix <gel11@ic.ac.uk>
2013-11-19 12:34:34 +0000

    fixxed testann

--- testANN.m
+++ testANN.m
@@ -1,13 +1,6 @@
 function [ expected ] = testANN( network, data )
-    expected = zeros(size(data, 2), 1);
     output = sim(network, data);
     corrected = bestMatch(output);
-    for i = 1:size(expected, 1)
-        for j = 1:6
-            if (corrected(j, i) == 1)
-                expected(i, 1) = j;
-            end
-        end
-    end
+    expected = NNout2labels(corrected);
 end
 
