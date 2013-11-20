8d256153f3bb47ae1a7d4884c095b54bcff3f3e6
Will de Renzy-Martin <wrd11@.ic.ac.uk>
2013-11-20 17:40:47 +0000

    mixed up some of my indexes...

--- crossValidateANN.m
+++ crossValidateANN.m
@@ -2,7 +2,7 @@ function [ rates ] = crossValidateANN( labels, examples, one_or_six,optimise_par
     folds = fold(labels,examples);
     rates = zeros(length(folds));
     if strcmp(optimise_parameters,'yes')
-        [topology,train_fn,learn_rate,momentum,activation_fn,epochs] = paramOpt(folds(1).train,folds(1).validate);
+        [topology,train_fn,learn_rate,momentum,activation_fn,epochs] = paramOpt(folds.train(1),folds.validate(1));
     else
         topology = params{1};
         train_fn = params{2};
@@ -11,12 +11,14 @@ function [ rates ] = crossValidateANN( labels, examples, one_or_six,optimise_par
         activation_fn = params{5};
         epochs = params{6};
     end
-    for i = 1:length(folds)
-        x = folds(i).train.examples;
-        y = folds(i).train.labels;
+    for i = 1:size(folds.train,2)
+        x = folds.train(i).examples;
+        y = folds.train(i).labels;
         network = createNN(topology,train_fn,learn_rate,momentum,activation_fn,epochs,x,y,one_or_six);
-        predictions = testANN(network,folds(i).examples);
-        rates(i) = classificationRate(predictions,folds(i).test.labels);
+        test_examples = folds.test(i).examples;
+        test_labels = folds.test(i).labels;
+        predictions = testANN(network,test_examples');
+        rates(i) = classificationRate(predictions,test_labels);
     end
 end
 
