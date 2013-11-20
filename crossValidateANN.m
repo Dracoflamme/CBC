113269553f0d3f1ded7c457a97046cd6a92914e0
Will de Renzy-Martin <wrd11@.ic.ac.uk>
2013-11-20 17:29:05 +0000

    cross validate ANN should be working
    you can optimise parameters or just give it some to use

---
+++ crossValidateANN.m
@@ -0,0 +1,22 @@
+function [ rates ] = crossValidateANN( labels, examples, one_or_six,optimise_parameters,params)
+    folds = fold(labels,examples);
+    rates = zeros(length(folds));
+    if strcmp(optimise_parameters,'yes')
+        [topology,train_fn,learn_rate,momentum,activation_fn,epochs] = paramOpt(fold(1).train,fold(1).validate);
+    else
+        topology = params(1);
+        train_fn = params(2);
+        learn_rate = params(3);
+        momentum = params(4);
+        activation_fn = params(5);
+        epochs = params(6);
+    end
+    for i = 1:length(folds)
+        x = folds(i).train.examples;
+        y = folds(i).train.labels;
+        network = createNN(topology,train_fn,learn_rate,momentum,activation_fn,epochs,x,y,one_or_six);
+        predictions = testANN(network,fold(i).examples);
+        rates(i) = classificationRate(predictions,fold(i).test.labels);
+    end
+end
+
