e27d91fdbf60f96fea49a2e60c1905be4c76326c
Guillaume Lacroix <gel11@ic.ac.uk>
2013-11-29 17:55:22 +0000

    KKK!!

---
+++ CBR/KNN.m
@@ -0,0 +1,14 @@
+function [ bestCase ] = KNN( target, cases, k )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+    bestCase = cases(1);
+    bestSimilarity = similarity1(bestCase, target);
+    for i=2 : length(cases)
+        thisCase = cases(i);
+        similarity = similarity1(thisCase, target);
+        if similarity>bestSimilarity
+            bestCase = thisCase;
+            bestSimilarity = similarity;
+        end
+    end 
+end
