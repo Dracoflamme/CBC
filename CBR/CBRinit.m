54f34af420da397703838f6387c1f8311b3de7f4
Conrad Godfrey <conradjgodfrey@tiscali.co.uk>
2013-11-29 17:04:27 +0000

    Implemented CBRinit function that takes examples,labels, and returns an array of cases

---
+++ CBR/CBRinit.m
@@ -0,0 +1,12 @@
+function [ cbr ] = CBRinit(x, y)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+    cbr = [];
+    for auv = 1:length(x)
+        active_aus = toAU(x(auv));
+        label = y(auv);
+        c = createCaseStruct(active_aus,label);
+        addCase(c,cbr);
+    end
+end
+
