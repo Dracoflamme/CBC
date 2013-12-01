071c5c67532085ec73dec847ec3fa2cd3e854aba
wrd11 <will.derenzymartin@gmail.com>
2013-12-01 23:30:30 +0000

    got rid of useless line

--- CBR/tmp_knn.m
+++ CBR/tmp_knn.m
@@ -5,7 +5,6 @@ function [neighbours] = knn(target,similarity, k, cases)
         cases_map(i,2)= feval(similarity,cases(i),target);
     end
     cases_map = sortrows(cases_map,2);
-    cases_map
     for j = 1:k
         neighbours(j) = cases(cases_map(end-j,1));
     end
