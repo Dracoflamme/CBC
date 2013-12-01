72460306f5af75ce9e6c85b579afabb07d58d7f1
wrd11 <will.derenzymartin@gmail.com>
2013-12-01 23:24:02 +0000

    tmp_knn now returns the k nearest neighbours

--- CBR/tmp_knn.m
+++ CBR/tmp_knn.m
@@ -1,10 +1,12 @@
 function [neighbours] = knn(target,similarity, k, cases)
-    cases_map = {};
+    cases_map = [];
     for i = 1:length(cases)
-        cases_map{i} = [cases(i) , feval(similarity,cases(i),target)];
+        cases_map(i,1)= i; 
+        cases_map(i,2)= feval(similarity,cases(i),target);
     end
-    cases_map = sortrows(cases_map,2); 
+    cases_map = sortrows(cases_map,2);
+    cases_map
     for j = 1:k
-        neighbours(i) = cases_map(i);
+        neighbours(j) = cases(cases_map(end-j,1));
     end
 end
